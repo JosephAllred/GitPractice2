353452227913af3451dd64d9d3c43dacdb34486d
lucawilkins <wilkinslt21@gcc.edu>
2023-03-02 23:13:29 -0500

    greeting user

--- src/GitPractice2.java
+++ src/GitPractice2.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class GitPractice2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = (scnr.next());
+        System.out.println("Hello, " + name + "!");
     }
 }
